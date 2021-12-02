ced0d4f5d8bcf5b7db16a1e9bae73b22a744dbc0
Toshiki Muromachi <ageki104@gmail.com>
2021-12-02 11:36:11 +0900

    model fix

--- src/models.py
+++ src/models.py
@@ -1,27 +1,29 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 # モデルの定義
 class Net(nn.Module):
     def __init__(self, classes):
-        super().__init__()
-        self.conv1 = torch.nn.Sequential(nn.Conv2d(3, 16, 3, 2, 1),
-                                         nn.BatchNorm2d(16),
-                                         nn.ReLU())
-        self.conv2 = torch.nn.Sequential(nn.Conv2d(16, 64, 3, 2, 1),
-                                         nn.BatchNorm2d(64),
-                                         nn.ReLU())
-
-        self.fc1 = nn.Linear(3136, 100)
-        self.dropout = nn.Dropout(0.5)
-        self.fc2 = torch.nn.Linear(100, classes)
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.dropout1 = nn.Dropout(0.25)
+        self.dropout2 = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(12*12*64, 128)
+        self.fc2 = nn.Linear(128, classes)
 
     def forward(self, x):
         x = self.conv1(x)
+        x = F.relu(x)
         x = self.conv2(x)
-        x = x.view(x.size(0), -1)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+        x = torch.flatten(x, 1)
         x = self.fc1(x)
-        x = self.dropout(x)
-        x = self.fc2(x)
-        return x
+        x = F.relu(x)
+        x = self.dropout2(x)
+        output = self.fc2(x)
+        return output
